1a57eec52d8ffe020cd85e303113d83160e4bbdb
larspeelman <lars.peelman@hotmail.com>
2018-11-15 13:46:13 +0100

    refactor mistakes

--- ParkShark.Domain/Divisions/Repository/DivisionDbContext.cs
+++ ParkShark.Domain/Divisions/Repository/DivisionDbContext.cs
@@ -35,7 +35,17 @@ namespace ParkShark.Domain.Divisions.Repository
             modelBuilder.Entity<Division>()
                 .ToTable("Division", "Div")
                 .HasKey(e => e.GuidID);
+
+            modelBuilder.Entity<Division>()
+                .Property(d => d.GuidID).HasColumnName("Division_ID");
+            modelBuilder.Entity<Division>()
+                .Property(d => d.Director).HasColumnName("Division_Director");
+            modelBuilder.Entity<Division>()
+                .Property(d => d.Name).HasColumnName("Division_Name");
+            modelBuilder.Entity<Division>()
+                .Property(d => d.OriginalName).HasColumnName("Division_OrgName");
             base.OnModelCreating(modelBuilder);
+
         }
 
         
