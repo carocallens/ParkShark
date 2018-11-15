f917d3248db4fba18e3d7098235be2bd2448d066
larspeelman <lars.peelman@hotmail.com>
2018-11-15 10:19:30 +0100

    database setup script

---
+++ Scripts/DivisionScript.sql
@@ -0,0 +1,53 @@
+create schema Div
+go
+
+create table Div.Division(
+Division_ID int not null,
+Division_Name varchar (100) not null,
+Division_OrgName varchar(100) not null,
+Division_Director varchar(100) not null
+constraint Division_pk primary key (Division_ID))
+
+insert into Div.Division
+(
+Division_ID ,
+Division_Name ,
+Division_OrgName ,
+Division_Director 
+)
+values
+(
+1,
+'1testdivision',
+'1testorgdivision',
+'1willem'
+)
+
+insert into Div.Division
+(
+Division_ID ,
+Division_Name ,
+Division_OrgName ,
+Division_Director 
+)
+values
+(
+2,
+'2testdivision',
+'2testorgdivision',
+'2willem'
+)
+insert into Div.Division
+(
+Division_ID ,
+Division_Name ,
+Division_OrgName ,
+Division_Director 
+)
+values
+(
+3,
+'3testdivision',
+'3testorgdivision',
+'3willem'
+)
