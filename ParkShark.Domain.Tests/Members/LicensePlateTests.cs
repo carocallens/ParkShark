8d50e6b9d5cabd613ea2716773343b3d8b97a008
WillemPolfliet <willem.polfliet@cegeka.com>
2018-11-19 14:52:23 +0100

    adjusting all

--- ParkShark.Domain.Tests/Members/LicensePlateTests.cs
+++ ParkShark.Domain.Tests/Members/LicensePlateTests.cs
@@ -1,23 +1,23 @@
-﻿//using ParkShark.Domain.Members;
-//using System;
-//using Xunit;
+﻿using ParkShark.Domain.Members;
+using System;
+using Xunit;
 
-//namespace ParkShark.Domain.Tests.Members
-//{
-//    public class LicensePlateTests
-//    {
-//        [Fact]
-//        public void GivenHappyPath_WhenCreateNewLicensePlate_ThenLicensePlateCreated()
-//        {
-//            //given
-//            var firstName = "firstName";
-//            var lastName = "lastName";
+namespace ParkShark.Domain.Tests.Members
+{
+    public class LicensePlateTests
+    {
+        [Fact]
+        public void GivenHappyPath_WhenCreateNewLicensePlate_ThenLicensePlateCreated()
+        {
+            //given
+            var firstName = "firstName";
+            var lastName = "lastName";
 
-//            var city = new City(
-//                        1234,
-//                        "CityName",
-//                        "CountryName"
-//                        );
+            var city = new City(
+                        1234,
+                        "CityName",
+                        "CountryName"
+                        );
 
             Address address = Address.CreateAddress(
                                 "StreetName",
@@ -33,46 +33,86 @@
                             memLevel
                             );
 
-//            //when
-//            var licensePlate = LicensePlate.CreateLicensePlate(member.MemberId,
-//                                                                "1-ABC-123",
-//                                                                "Belgium");
-
-//            //then
-//            Assert.NotNull(licensePlate);
-//        }
-
-//        [Fact]
-//        public void GivenLicensePlateWithLicensePlateValueNull_WhenCreateLicensePlate_ThenLicensePlateIsNull()
-//        {
-//            //given
-//            var firstName = "firstName";
-//            var lastName = "lastName";
-
-//            var city = new City(
-//                        1234,
-//                        "CityName",
-//                        "CountryName"
-//                        );
-
-//            Address address = Address.CreateAddress(
-//                                "StreetName",
-//                                "StreetNumber",
-//                                city.ZIP
-//                                );
-
-//            var member = Member.CreateMember(
-//                            firstName,
-//                            lastName,
-//                            address
-//                            );
+            //when
+            var licensePlate = LicensePlate.CreateLicensePlate(member.MemberId,
+                                                                "1-ABC-123",
+                                                                "Belgium");
 
             //then
+            Assert.NotNull(licensePlate);
+        }
+
+        [Fact]
+        public void GivenLicensePlateWithLicensePlateValueNullwhut_WhenCreateLicensePlate_ThenLicensePlateIsNull()
+        {
+            //given
+            var firstName = "firstName";
+            var lastName = "lastName";
+
+            var city = new City(
+                        1234,
+                        "CityName",
+                        "CountryName"
+                        );
+
+            Address address = Address.CreateAddress(
+                                "StreetName",
+                                "StreetNumber",
+                                city.ZIP
+                                );
+
+            var member = Member.CreateMember(
+                            firstName,
+                            lastName,
+                            address,
+                            MembershipLevelEnum.Bronze,
+                            new MembershipLevel()
+                            );
+
+            var licensePlate = LicensePlate.CreateLicensePlate(member.MemberId, null, "Belgium");
+
             Assert.Null(licensePlate);
         }
 
         [Fact]
         public void GivenLicensePlateWithLicensePlateValueNull_WhenCreateLicensePlate_ThenLicensePlateIsNull()
+        {
+
+            var firstName = "firstName";
+            var lastName = "lastName";
+
+            var city = new City(
+                        1234,
+                        "CityName",
+                        "CountryName"
+                        );
+
+            Address address = Address.CreateAddress(
+                                "StreetName",
+                                "StreetNumber",
+                                city.ZIP
+                                );
+
+            var memLevel = new MembershipLevel();
+            var member = Member.CreateMember(
+                            firstName,
+                            lastName,
+                            address,
+                            MembershipLevelEnum.Bronze,
+                            memLevel
+                            );
+
+
+            var licensePlate = LicensePlate.CreateLicensePlate(member.MemberId,
+                                                                null,
+                                                                "Belgium");
+
+
+            Assert.Null(licensePlate);
+        }
+
+        [Fact]
+        public void GivenLicensePlateWithIssueingCountryNull_WhenCreateLicensePlate_ThenLicensePlateIsNull()
         {
             //given
             var firstName = "firstName";
@@ -101,48 +141,11 @@
 
             //when
             var licensePlate = LicensePlate.CreateLicensePlate(member.MemberId,
-                                                                null,
-                                                                "Belgium");
+                                                                "1-ABC-123",
+                                                                null);
 
-//            //then
-//            Assert.Null(licensePlate);
-//        }
-
-//        [Fact]
-//        public void GivenLicensePlateWithIssueingCountryNull_WhenCreateLicensePlate_ThenLicensePlateIsNull()
-//        {
-//            //given
-//            var firstName = "firstName";
-//            var lastName = "lastName";
-
-//            var city = new City(
-//                        1234,
-//                        "CityName",
-//                        "CountryName"
-//                        );
-
-//            Address address = Address.CreateAddress(
-//                                "StreetName",
-//                                "StreetNumber",
-//                                city.ZIP
-//                                );
-
-            var memLevel = new MembershipLevel();
-            var member = Member.CreateMember(
-                            firstName,
-                            lastName,
-                            address,
-                            MembershipLevelEnum.Bronze,
-                            memLevel
-                            );
-
-//            //when
-//            var licensePlate = LicensePlate.CreateLicensePlate(member.MemberId,
-//                                                                "1-ABC-123",
-//                                                                null);
-
-//            //then
-//            Assert.Null(licensePlate);
-//        }
-//    }
-//}
+            //then
+            Assert.Null(licensePlate);
+        }
+    }
+}
